94bd43fc2cea47b479ac58744579ef4d58fd210a
squeek <squeek502@hotmail.com>
2014-03-25 13:29:37 -0700

    Fix version

--- java/squeek/tictooltips/ModTiCTooltips.java
+++ java/squeek/tictooltips/ModTiCTooltips.java
@@ -14,7 +14,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 @Mod(modid = ModTiCTooltips.MODID, version = ModTiCTooltips.VERSION, dependencies = "required-after:TConstruct;after:ExtraTiC;after:TSteelworks")
 public class ModTiCTooltips {
     public static final String MODID = "TiCTooltips";
-    public static final String VERSION = "%VERSION%";
+    public static final String VERSION = "1.0";
 
     @SideOnly(Side.CLIENT)
     @EventHandler
