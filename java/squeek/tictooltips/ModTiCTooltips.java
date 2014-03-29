673075209134682ab82ce52b999a6caccea5aef8
squeek <squeek502@hotmail.com>
2014-03-28 19:42:21 -0700

    Version bump

--- java/squeek/tictooltips/ModTiCTooltips.java
+++ java/squeek/tictooltips/ModTiCTooltips.java
@@ -14,7 +14,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 @Mod(modid = ModTiCTooltips.MODID, version = ModTiCTooltips.VERSION, dependencies = "required-after:TConstruct;after:ExtraTiC;after:TSteelworks")
 public class ModTiCTooltips {
 	public static final String MODID = "TiCTooltips";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 
 	@SideOnly(Side.CLIENT)
 	@EventHandler
