3836b0af5d0baa0dca54f786e33a686cc9ef2de0
squeek <squeek502@hotmail.com>
2014-03-30 07:50:43 -0700

    Use gradle to replace version

--- java/squeek/tictooltips/ModTiCTooltips.java
+++ java/squeek/tictooltips/ModTiCTooltips.java
@@ -15,7 +15,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ModTiCTooltips
 {
 	public static final String MODID = "TiCTooltips";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "${version}";
 
 	@SideOnly(Side.CLIENT)
 	@EventHandler
